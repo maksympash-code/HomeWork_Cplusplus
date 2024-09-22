d779b2074df021d0d82bbbadfabd2215739f99b0
maksympash <maksympash@knu.ua>
2024-09-22 22:12:05 +0300

    HW_6

--- HW_6/6.22.cpp
+++ HW_6/6.22.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-unsigned int LowerBits(unsigned int n, int k) {
-    unsigned int lower_k = (1 << k) - 1;
+ int LowerBits(int n, int k) {
+    int lower_k = (1 << k) - 1;
     return n | ~lower_k;
 }
 
-int main() {
-    unsigned int n;
-    int k;
-
-    cout << "Enter n: ";
-    cin >> n;
-    cout << "Enter k: ";
-    cin >> k;
-
-    unsigned int result = LowerBits(n, k);
-
-    cout << "Result: " << result << endl;
-
-    return 0;
-}
+// int main() {
+//     int n;
+//     int k;
+//
+//     cout << "Enter n: ";
+//     cin >> n;
+//     cout << "Enter k: ";
+//     cin >> k;
+//
+//     int result = LowerBits(n, k);
+//
+//     cout << "Result: " << result << endl;
+//
+//     return 0;
+// }
